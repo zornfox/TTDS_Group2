b43174ac7d2aaa07796d7c72dcceb971cca9b062
hubertzhong <60122586+hubertzhong@users.noreply.github.com>
2021-03-08 16:37:36 +0000

    Revert "Retrieve urls"
    This reverts commit 407693d4b30526f6fe8d81d7e9bc604901abdded.

--- backend/run_backend_example.py
+++ backend/run_backend_example.py
@@ -8,10 +8,8 @@ m.prepare_model()
 
 # run this for any incoming claims
 claim="coronavirus is not a virus but a bacteria"
-articles_text, articles_urls=m.retrieve_documents(claim)
+articles=m.retrieve_documents(claim)
 
-for i in range(5):
-    text=articles_text[i]
-    url=articles_urls[i]
-    print(text,url)
+for x in articles[0:5]:
+    print(x)
     print("-"*10)
